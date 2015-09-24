aef2c12e91f6b397a616232522b13ba4560f6dbe
jason3e7 <jason3e7@gmail.com>
2015-09-24 23:24:33 +0800

    AC

---
+++ b428.c
@@ -0,0 +1,21 @@
+/*
+ * @file d428.c
+ * @author Jason3e7
+ * @date 201509242318
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char a[1000], b[1000];
+	int output;
+	while(scanf("%s", &a) != EOF) {
+		scanf("%s", &b);
+		output = b[0] - a[0];
+		if (output < 0)
+			output += 26;
+		printf("%d\n", output);
+	}
+	return 0;
+}
