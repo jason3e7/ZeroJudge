396302acb0416bdaba7947a391c8e00f6a34f917
jason3e7 <jason3e7@gmail.com>
2015-10-27 16:33:28 +0800

    Use getchar not AC

--- d681.c
+++ d681.c
@@ -5,49 +5,50 @@
  */
 
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-	char bin[6], line[1000];
-	int i, len, flag, locate;
-	while(scanf(" %[^\n]", &line) != EOF) {
-		for (i = 0; i < 5; i++) {
-			printf("%c", line[i]);
-			bin[i] = line[i];
-		}
-		bin[5] = '\0';
-		locate = 0;
-		len = strlen(line);
-		for (i = 5; i < len; i++) {
-			if (line[i] != ' ') {
-				if (line[i] == '1' || line[i] == '0') {
-					printf("%c", line[i]);
-					if (flag == 1) {
-						if (bin[locate] == '1' && line[i] == '1')
-							bin[locate] = '1';
-						else
-							bin[locate] = '0';
-					} else if (flag == 2) {	
-						if (bin[locate] == '1' || line[i] == '1')
-							bin[locate] = '1';
-						else
-							bin[locate] = '0';
-					}
-					locate++;
-				} else if (line[i] == 'a'){
-					printf("&&");
-					i += 2;
-					flag = 1;
-					locate = 0;
-				} else if (line[i] == 'o') {
-					printf("||");
-					i += 1;
-					flag = 2;
-					locate = 0;
-				}
+	char bin[6], temp;
+	int i, flag, locate = 0, count = 0;
+	while(temp = getchar()) {
+		if (temp == EOF)
+			break;
+		if (temp == '\n') {
+			printf(" = ");
+			for (i = 0; i < 5; i++) { 
+				printf("%c", bin[i]);
 			}
-		}
-		printf(" = %s\n", bin);
+			printf("\n");
+			count = 0;
+			locate = 0;
+		} else if (count < 5) {
+			printf("%c", temp);
+			bin[count] = temp;
+			count++;
+		} else if (temp != ' ') {
+			if (temp == '1' || temp == '0') {
+				printf("%c", temp);
+				if (flag == 1) {
+					if (bin[locate] == '1' && temp == '1')
+						bin[locate] = '1';
+					else
+						bin[locate] = '0';
+				} else if (flag == 2) {	
+					if (bin[locate] == '1' || temp == '1')
+						bin[locate] = '1';
+					else
+						bin[locate] = '0';
+				}
+				locate++;
+			} else if (temp == 'a'){
+				printf("&&");
+				flag = 1;
+				locate = 0;
+			} else if (temp == 'o') {
+				printf("||");
+				flag = 2;
+				locate = 0;
+			}
+		}	
 	}
 	return 0;
 }
