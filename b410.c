dfb16aa92698d8cf03f5b41b854b84392de769fd
jason3e7 <jason3e7@gmail.com>
2015-11-17 10:53:35 +0800

    b410 AC

---
+++ b410.c
@@ -0,0 +1,16 @@
+/*
+ * @file d410.c
+ * @author Jason3e7
+ * @date 201511171046
+ */
+
+#include <stdio.h>
+
+int main() {
+	int n;
+	while(scanf("%d", &n) != EOF)
+		while(n--)
+			printf("HELLO!\n");
+	return 0;
+}
+
