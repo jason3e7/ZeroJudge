b0af5b16a0918547b5bd93593c54be32a22194d1
jason3e7 <jason3e7@gmail.com>
2015-11-26 11:17:18 +0800

    d762 AC

--- d762.c
+++ d762.c
@@ -7,40 +7,27 @@
 
 #include <stdio.h>
 
-int num[5], isUse[5] = {0};
-char symbol[5];
+int num[5], isUse[5] = {0}, find;
 
-int DFS(int count, int sum) {
-	int i, j;
+void DFS(int count, int sum) {
+	int i;
+	if (find == 1)
+		return;
 	if (count == 6) {
-		if (sum == 23) {
-			for (i = 1; i <= 5; i++) {
-				for (j = 0; j < 5; j++) {
-					if (i == isUse[j]) {
-						printf("%d", num[j]);
-					}
-				}
-				if (i != 5)
-					printf(" %c ", symbol[i + 1]);
-			}
-			printf("\n");
-		}
-		return 0;	
+		if (sum == 23)
+			find = 1;
+		return;
 	}
 	for (i = 0; i < 5; i++) {
 		if (isUse[i] == 0) {
 			isUse[i] = count;
-			symbol[count] = '+';
 			DFS(count + 1, sum + num[i]);
-			symbol[count] = '-';
 			DFS(count + 1, sum - num[i]);
-			symbol[count] = '*';
 			DFS(count + 1, sum * num[i]);
-			symbol[count] = ' ';
 			isUse[i] = 0;
 		}
 	}
-	return 1;
+	return;
 }
 
 int main() {
@@ -53,23 +40,18 @@ int main() {
 		if (flag == 1)
 			break;
 		
-		//flag = 0;
+		find = 0;
 		for (i = 0; i < 5; i++) {
 			isUse[i] = 1;
-			//flag = DFS(1, num[i]);
 			DFS(2, num[i]);
-			isUse[i] = 0;
-			/*
-			if (flag == 1)
+			isUse[i] = 0;	
+			if (find == 1)
 				break;
-			*/
 		}
-		/*
-		if (DFS() == 1)	
+		if (find == 1)	
 			printf("Possible\n");
 		else
 			printf("Impossible\n");
-		*/
 	}
 	return 0;
 }
