148d78e0eb2b67f24045168575fa03a3ddeea818
jason3e7 <jason3e7@gmail.com>
2015-11-30 14:43:30 +0800

    d412 AC

--- d412.c
+++ d412.c
@@ -6,12 +6,9 @@
   */
 
 #include <stdio.h>
-#include <sys/time.h>
 
-struct timeval stop, start;
-
-char operator[100];
-int p, num[102], find, vis[102][64005];
+char operator[100], vis[102][64005];
+int p, num[102], find;
 
 void DFS(int locate, int sum) {
 	int i;
@@ -32,10 +29,11 @@ void DFS(int locate, int sum) {
 
 	if (!(32000 >= sum && sum >= -32000)) 
 		return;
+
 	if (vis[locate][sum + 32000] == 1)
 		return;
-	
 	vis[locate][sum + 32000] = 1;
+	
 	operator[locate] = '+';
 	DFS(locate + 1, sum + num[locate]);	
 	operator[locate] = '-';
@@ -51,7 +49,6 @@ void DFS(int locate, int sum) {
 int main() {
 	int i, j, n;
 	scanf("%d", &n);
-	gettimeofday(&start, NULL);
 	while(n--) {
 		find = 0;
 		scanf("%d", &p);
@@ -66,19 +63,6 @@ int main() {
 		if (find == 0)
 			printf("NO EXPRESSION\n");
 	}
-	gettimeofday(&stop, NULL);
-	printf("took %lu\n", stop.tv_usec - start.tv_usec);
-	/*
-	for (i = 0; i < 102; i++) {
-		for (j = 0; j < 64005; j++) {
-			if (vis[i][j] == 1) {
-				printf("%d : %d\n", i, j + 32000);
-			}
-		}
-	}
-	*/
 	return 0;
 }
 
-// test case
-// 1 93 20541 12372 26460 8844 26376 3402 3743 16201 10406 18682 23805 10468 1173 14050 18140 13499 7059 13304 26112 510 16810 30787 4728 3764 7543 7128 4693 9875 13293 22817 2920 6185 7540 29379 15029 1915 5132 23123 18116 19889 9804 14272 30356 15328 673 20847 1179 7731 6503 27290 8241 27664 30428 17320 31427 5970 28799 4119 20197 10091 31288 23116 20627 6827 24846 8007 8742 2329 31129 31209 22217 8933 17832 24925 24260 18505 13771 25438 30587 24625 25080 11179 20288 27859 28498 24066 1829 29648 28185 26377 7738 31824 17492 717
