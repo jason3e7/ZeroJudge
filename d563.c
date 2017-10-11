9bee685361779723e00c13ec2b6e899d674ba17f
jason3e7 <jason3e7@gmail.com>
2017-10-12 11:33:25 +1300

    d563 WA

--- d563.c
+++ d563.c
@@ -6,15 +6,43 @@
   */
 
 #include <stdio.h>
+
+void clean_stdin(void) {
+    char c;
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
+void getLineNumber(int* number) {
+	int temp = 0, flag = 0, point = 0;
+	char c;
+    do {
+		c = getchar();
+		if('0' <= c && c <= '9') {
+        	temp *= 10;
+			temp += (c - '0');
+			flag = 1;
+		} else {
+			if(flag == 1) {
+				number[point] = temp;
+				temp = 0;
+				flag = 0; 
+				point++;
+			}
+		}
+    } while (c != '\n' && c != EOF); 
+}
+
 int main() {
 	int n, number[100001], i, j, sum, begin, end;
+	char c;
 	while(scanf("%d", &n) != EOF) {
 		sum = 0;
 		begin = 0;
 		end = 0;
-		for(i = 0; i < n; i++) {
-			scanf("%d", &number[i]);
-		}
+		clean_stdin();
+		getLineNumber(&number[0]);
 		for(i = 0; i < n; i++) {
 			begin += number[i];
 			end = 0;
