bfef2c870dd132e32a1684d0ec5ce3fdb59a5eca
jason3e7 <jason3e7@gmail.com>
2017-10-18 17:02:28 +1300

    b542 TLE

--- b542.c
+++ b542.c
@@ -8,37 +8,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
 int main() {
-	int n, q, i, j, k, high[100001], diff[100001], point, temp, match;
+	int n, q, high[100001], i, j, temp, count, match;
 	while(scanf("%d %d", &n, &q) != EOF) {
-		for(i = 0; i < 100001; i++) {
-			diff[i] = 0;
-		}
-		point = 0;
 		for(i = 0; i < n; i++) {
 			scanf("%d", &high[i]);
-			for(j = i - 1; j >= 0; j--) {
-				temp = abs(high[i] - high[j]);
-				match = 0;
-				for(k = 0; k < point; k++) {
-					if(temp == diff[k]) {
-						match = 1;
-						break;
-					}
-				}
-				if(match == 0) {
-					diff[point] = temp;
-					point++;
-				}
-			}
-		} 
+		}
+		qsort(high, n, sizeof(int), cmpfunc); 
+		for(i = 0; i + 1 < n; i++) {
+			high[i] = high[i + 1] - high[i]; 
+		}
+		n--;
+		
 		while(q--) {
 			scanf("%d", &temp);
 			match = 0;
-			for(k = 0; k < point; k++) {
-				if(temp == diff[k]) {
-					printf("YES\n");
-					match = 1;
+			for(i = 0; i < n; i++) {
+				count = 0;
+				for(j = i; j < n; j++) {
+					count += high[j];
+					if(count == temp) {
+						printf("YES\n");
+						match = 1;
+						break;
+					}
+					if(count > temp) {
+						break;
+					}
+				}
+				if(match == 1) {
 					break;
 				}
 			}
