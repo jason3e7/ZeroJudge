80a62b4149e46717edaba66be8b8985e5daa5e09
jason3e7 <jason3e7@gmail.com>
2017-10-21 00:27:42 +1300

    d550 AC

--- d550.c
+++ d550.c
@@ -17,28 +17,30 @@ void swap(int m, int a[m], int b[m]) {
 }
 
 int main() {
-	int i, j, k, n, m, number[10001][201];
+	int i, j, k, n, m, number[10001][201], max;
 	while(scanf("%d %d", &n, &m) != EOF) {
 		for(i = 0; i < n; i++) {
 			for(j = 0; j < m; j++) {
 				scanf("%d", &number[i][j]);
 			}
 		}
-		for(i = 1; i < n; i++) {
-			for(j = 0; j < (n - i); j++) {
+		for(i = 0; i < n; i++) {
+			max = 0;
+			for(j = n - 1 - i; j > 0; j--) {
 				for(k = 0; k < m; k++) {
-					if(number[j][k] < number[j + 1][k]) {
+					if(number[max][k] > number[j][k]) {
 						break;
 					}
-					if(number[j][k] == number[j + 1][k]) {
+					if(number[max][k] == number[j][k]) {
 						continue;
 					}
-					if(number[j][k] > number[j + 1][k]) {
-						swap(m, number[j], number[j + 1]);
+					if(number[max][k] < number[j][k]) {
+						max = j;
 						break;
 					}
 				}
 			}
+			swap(m, number[max], number[n - 1 - i]);
 		}
 
 		for(i = 0; i < n; i++) {
