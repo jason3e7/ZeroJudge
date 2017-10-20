3ef9a81dac3afc22bdaf500680c51350b13ea16e
jason3e7 <jason3e7@gmail.com>
2017-10-21 12:16:44 +1300

    a847 AC

--- a847.c
+++ a847.c
@@ -3,6 +3,7 @@
   * @author Jason3e7
   * @algorithm DFS
   * @date 201710211111
+  * @note BFS better
   */
 
 #include <stdio.h>
@@ -11,10 +12,6 @@ int route[26][26], map[26], begin, end;
 
 int walk(int point) {
 	int i;
-	for(i = 0; i < 26; i++) {
-		printf("%d ", map[i]);
-	}
-	printf("\n");
 	if(point == end) {
 		return 0;
 	}
@@ -41,28 +38,20 @@ int main() {
 			for(j = 0; j < 26; j++) {
 				route[i][j] = -1;
 			}
-			map[i] = 100;
+			map[i] = 1000000;
 		}
 		for(i = 0; i < n; i++) {
 			scanf("%s %s %d", x, y, &t);
 			route[x[0] - 'A'][y[0] - 'A'] = t;
 			route[y[0] - 'A'][x[0] - 'A'] = t;
 		}
-		
-		for(i = 0; i < 26; i++) {
-			for(j = 0; j < 26; j++) {
-				printf("%d ", route[i][j] % 100);
-			}
-			printf("\n");
-		}
-		
 		scanf("%s %s", x, y);
 		begin = x[0] - 'A';
 		end = y[0] - 'A';
 		map[begin] = 0;
 		out = walk(begin);
-		if(map[end] == 100) {
-			printf("NoRoute!\n");
+		if(map[end] == 1000000) {
+			printf("NoRoute\n");
 		} else {
 			printf("%d\n", map[end]);
 		}
