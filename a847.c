60808f4b842f266d29e57b68812d54195a44a273
jason3e7 <jason3e7@gmail.com>
2017-10-21 12:11:01 +1300

    a847 print all

---
+++ a847.c
@@ -0,0 +1,72 @@
+ /*
+  * @file a847.c
+  * @author Jason3e7
+  * @algorithm DFS
+  * @date 201710211111
+  */
+
+#include <stdio.h>
+
+int route[26][26], map[26], begin, end;
+
+int walk(int point) {
+	int i;
+	for(i = 0; i < 26; i++) {
+		printf("%d ", map[i]);
+	}
+	printf("\n");
+	if(point == end) {
+		return 0;
+	}
+	for(i = 0; i < 26; i++) {
+		if(route[point][i] == -1) {
+			continue;
+		}
+		if((map[point] + route[point][i]) > map[end]) {
+			continue;	
+		}
+		if((map[point] + route[point][i]) < map[i]) {
+			map[i] = map[point] + route[point][i];
+			walk(i);
+		}
+	}
+	return 0;
+}
+
+int main() {
+	int n, t, i, j, out;
+	char x[5], y[5];
+	while(scanf("%d", &n) != EOF) {
+		for(i = 0; i < 26; i++) {
+			for(j = 0; j < 26; j++) {
+				route[i][j] = -1;
+			}
+			map[i] = 100;
+		}
+		for(i = 0; i < n; i++) {
+			scanf("%s %s %d", x, y, &t);
+			route[x[0] - 'A'][y[0] - 'A'] = t;
+			route[y[0] - 'A'][x[0] - 'A'] = t;
+		}
+		
+		for(i = 0; i < 26; i++) {
+			for(j = 0; j < 26; j++) {
+				printf("%d ", route[i][j] % 100);
+			}
+			printf("\n");
+		}
+		
+		scanf("%s %s", x, y);
+		begin = x[0] - 'A';
+		end = y[0] - 'A';
+		map[begin] = 0;
+		out = walk(begin);
+		if(map[end] == 100) {
+			printf("NoRoute!\n");
+		} else {
+			printf("%d\n", map[end]);
+		}
+	}
+	return 0;
+}
+
