473a004188efb32e30428ce5a68940f9b85cf2f7
jason3e7 <jason3e7@gmail.com>
2017-10-26 11:36:55 +1300

    d643 print dp

--- d643.c
+++ d643.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-int n, v[10001], s[10001], dp[10001][100];
+int n, v[10001], s[10001], dp[10001][101];
 
 int max(int a, int b) {
 	if(a > b) {
@@ -23,7 +23,7 @@ int findMax(int point, int existV) {
 	if(point == 0) {
 		return 0;
 	}
-	if(dp[point][existV] == 0) {
+	if(dp[point][existV] == -1) {
 		dp[point][existV] = max(findMax(point - 1, existV), findMax(point - 1, existV - v[point]) + s[point]);
 	}
 	return dp[point][existV];
@@ -35,12 +35,19 @@ int main() {
 		for(i = 1; i <= n; i++) {
 			scanf("%d %d", &v[i], &s[i]);
 		}
-		for(i = 0; i < 10001; i++) {
+		for(i = 1; i <= n; i++) {
 			for(j = 0; j < 101; j++) {
-				dp[i][j] = 0;
+				dp[i][j] = -1;
 			}
 		}
 		printf("%d\n", findMax(n, 100));
+		for(i = 1; i <= n; i++) {
+			for(j = 0; j < 101; j++) {
+				if(dp[i][j] != -1) {
+					printf("%d %d %d\n", i, j, dp[i][j]);
+				}
+			}
+		}
 	}
 	return 0;
 }
