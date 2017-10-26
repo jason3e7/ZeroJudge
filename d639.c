36b5e61eadecf758b57e9613121565cd948807c9
jason3e7 <jason3e7@gmail.com>
2017-10-26 14:17:40 +1300

    d639 AC

--- d639.c
+++ d639.c
@@ -38,15 +38,73 @@ A  * |a2| = | an + 2 |
   */
 
 #include <stdio.h>
+#include <string.h>
+ 
+int out[3][3], base[3][3];
+
+int baseDouble() {
+	int i, j, temp[3][3];
+	for(i = 0; i < 3; i++) {
+		for(j = 0; j < 3; j++) {
+			temp[i][j] = (base[i][0] * base[0][j]) % 10007;
+			temp[i][j] += (base[i][1] * base[1][j]) % 10007;
+			temp[i][j] += (base[i][2] * base[2][j]) % 10007;
+			temp[i][j] = temp[i][j] % 10007;
+		}
+	}
+	memcpy(base, temp, sizeof(temp));
+	return 0;
+}
+
+int matrixMuti() {
+	int i, j, temp[3][3];
+	for(i = 0; i < 3; i++) {
+		for(j = 0; j < 3; j++) {
+			temp[i][j] = (out[i][0] * base[0][j]) % 10007;
+			temp[i][j] += (out[i][1] * base[1][j]) % 10007;
+			temp[i][j] += (out[i][2] * base[2][j]) % 10007;
+			temp[i][j] = temp[i][j] % 10007;
+		}
+	}
+	memcpy(out, temp, sizeof(temp));
+	return 0;
+}
 
 int main() {
-	int n,  f[1001], i, j;
-	f[0] = 0, f[1] = 1, f[2] = 1, f[3] = 1;
-	for(i = 4; i < 1001; i++) {
-		f[i] = (f[i - 1] + f[i - 2] + f[i - 3]) % 10007;
-	}
+	int n, point, bit[32], i, first, j;
 	while(scanf("%d", &n) != EOF) {
-		printf("%d\n", f[n]);
+		if(n <= 3) {
+			printf("1\n");
+			continue;
+		}
+		n--;
+		base[0][0] = 1;
+		base[0][1] = 1;
+		base[0][2] = 1;
+		base[1][0] = 1;
+		base[1][1] = 0;
+		base[1][2] = 0;
+		base[2][0] = 0;
+		base[2][1] = 1;
+		base[2][2] = 0;
+		point = 0;
+		do {
+			bit[point] = n%2;
+			point++;
+		} while(n/=2);
+		first = 1;
+		for(i = 0; i < point; i++) {
+			if(bit[i] == 1) {
+				if(first == 1) {
+					memcpy(out, base, sizeof(base));
+					first = 0;
+				} else {
+					matrixMuti();
+				}
+			}
+			baseDouble();
+		}
+		printf("%d\n", (out[2][0] + out[2][1] + out[2][2]) % 10007);
 	}
 	return 0;
 }
