5e9edd9fd1c25bd29b8c4473797572fb7900c176
jason3e7 <jason3e7@gmail.com>
2017-10-28 10:54:11 +1300

    fix block getline.c

--- blocks/getline.c
+++ blocks/getline.c
@@ -30,14 +30,49 @@ char * fix_fgets(char * str, int n) {
 	return ret_val;
 }
 
+int getLineNumber(char line[], int number[], int* point) {
+	int length = strlen(line), flag = 0, count = 0, i;
+	for(i = 0; i < length; i++) {
+		if('0' <= line[i] && line[i] <= '9') {
+			count *= 10;
+			count += (line[i] - '0');
+			flag = 1;
+			if(i == length - 1) {
+				number[*point] = count;
+				(*point)++;
+			}
+		} else {
+			if(flag == 1) {
+				number[*point] = count;
+				(*point)++;
+				count = 0;
+				flag = 0;
+			}
+		}
+	}
+	if((*point) == 0) {
+		return 0;
+	}
+	return 1;
+}
+
 int main() {
 	char line[100001];
-	int length, n, i;
+	int length, n, i, num[101], point;
 	scanf("%d", &n);
 	printf("%d\n", n);
 	clean_stdin();
 	while(fix_fgets(line, 100001) != NULL) {
 		printf("%s\n", line);
+		point = 0;
+		if(getLineNumber(line, num, &point)) {
+			for(i = 0; i < point; i++) {
+				printf("%d,", num[i]);
+			}
+			printf("\n");
+		} else {
+			printf("No Number\n");
+		}
 	}
 	return 0;
 }
