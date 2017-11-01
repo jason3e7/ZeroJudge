29661bf053c386415712bd161684b781002cbc6d
jason3e7 <jason3e7@gmail.com>
2017-11-02 12:04:30 +1300

    d047 AC

--- d047.c
+++ d047.c
@@ -9,25 +9,13 @@
 #include <stdio.h>
 #include <string.h>
 
-char * fix_fgets(char * str, int n) {
-	char * ret_val;
-	int i;
-	ret_val = fgets(str, n, stdin);
-	if(ret_val) {
-		for(i = 0; i < n; i++) {
-			if(str[i] == '\n' || str[i] == '\r') {
-				str[i] = '\0';
-				break;
-			}
-		} 
-	}
-	return ret_val;
-}
-
 int main() {
 	char line[100001];
-	int length, i, number, flag, sum, bitDiff;
-	while(fix_fgets(line, 100001) != NULL) {
+	int length, i, number, flag, sum, bitDiff, first = 1;
+	while(scanf("%s", line) != EOF) {
+		if (first == 0) {
+			printf("\n");
+		}
 		length = strlen(line);
 		flag = 0;
 		sum = 0;
@@ -60,7 +48,7 @@ int main() {
 		if (flag == 0) {
 			printf("This is an ordinary year.\n");
 		}
-		printf("\n");
+		first = 0;
 	}
 	return 0;
 }
