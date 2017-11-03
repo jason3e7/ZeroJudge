ad5d1cd82bddd096c04b9148c3c15f28be31875e
jason3e7 <jason3e7@gmail.com>
2017-11-04 10:58:16 +1300

    d130 AC

--- d130.c
+++ d130.c
@@ -4,33 +4,31 @@
   * @algorithm  
   * @UVa 138
   * @date 201711041003
+  * @note while(1) Wrong answer Why !?
   */
 
 #include <stdio.h>
 
 int main() {
-	int n[10] = {0}, k[10] = {0}, point = 0, i;
-	int leftSum = 1, mid = 2, rightSum = 0, right;
-	while(1) {
-		right = mid + 1;
-		rightSum = 0;
+	int point, i;
+	unsigned long long int n[10] = {0}, k[10] = {0};
+	unsigned long long int leftSum = 1, mid = 2, rightSum = 3, rightEnd = 4;
+	while(point < 10) {
 		while(leftSum > rightSum) {
-			rightSum += right;
+			rightSum += rightEnd;
 			if (leftSum == rightSum) {
-				n[point] = right;
+				n[point] = rightEnd;
 				k[point] = mid;
 				point++;
 			}
-			right++;
+			rightEnd++;
 		}
 		leftSum += mid;
 		mid++;
-		if(point == 6) {
-			break;
-		}
+		rightSum -= mid;
 	}
 	for(i = 0; i < 10; i++) {
-		printf("%10d%10d\n", k[i], n[i]);
+		printf("%10llu%10llu\n", k[i], n[i]);
 	}
 	return 0;
 }
