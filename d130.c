854e8928294c5521c86d156daedc47460f351e24
jason3e7 <jason3e7@gmail.com>
2017-11-04 11:37:32 +1300

    d130 AC

--- d130.c
+++ d130.c
@@ -1,34 +1,31 @@
  /*
   * @file d130.c
   * @author Jason3e7
-  * @algorithm  
+  * @algorithm math 
   * @UVa 138
   * @date 201711041003
   * @note while(1) Wrong answer Why !?
+  * (1 + k - 1) * (k - 1) / 2 = (1 + n) * n / 2 - (1 + k - 1) * (k - 1) / 2 - k
+  * k * (k - 1)  = (1 + n) * n - k * (k - 1) - 2k 
+  * k * k - k = n * n + n - k * k + k - 2k 
+  * 2k * k = n * n + n
+  * k * k = (n * n + n)/2
   */
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-	int point, i;
-	unsigned long long int n[10] = {0}, k[10] = {0};
-	unsigned long long int leftSum = 1, mid = 2, rightSum = 3, rightEnd = 4;
-	while(point < 10) {
-		while(leftSum > rightSum) {
-			rightSum += rightEnd;
-			if (leftSum == rightSum) {
-				n[point] = rightEnd;
-				k[point] = mid;
-				point++;
-			}
-			rightEnd++;
+	int i;
+	unsigned long long int k, n = 8, temp;
+	for(i = 0; i < 10;) {
+		temp = (n * n + n) / 2;
+		k = (int)sqrt(temp);
+		if (k * k == temp) {
+			printf("%10llu%10llu\n", k, n);
+			i++;
 		}
-		leftSum += mid;
-		mid++;
-		rightSum -= mid;
-	}
-	for(i = 0; i < 10; i++) {
-		printf("%10llu%10llu\n", k[i], n[i]);
+		n++;
 	}
 	return 0;
 }
