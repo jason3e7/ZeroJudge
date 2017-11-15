22738de2a86bd6ddbdec9e8d9d8a36013dd699bb
jason3e7 <jason3e7@gmail.com>
2017-11-16 12:27:58 +1300

    d326 AC

---
+++ d326.c
@@ -0,0 +1,80 @@
+ /*
+  * @file d326.c
+  * @author Jason3e7
+  * @algorithm bitwise operator 
+  * @date 201711161150
+  */ 
+ 
+#include <stdio.h>
+#include <math.h>
+
+int set_bit(int v, int b) {
+	return (v | (int)pow(2, b));
+}
+
+int unset_bit(int v, int b) {
+	return v - (v & (int)pow(2, b));
+}
+
+int check_bit(int v, int a, int b) {
+	return ((v >> b) & 1);
+}
+
+void test() {
+	printf("%d\n", set_bit(1, 0));
+	printf("%d\n", set_bit(2, 0));
+	printf("%d\n", set_bit(3, 0));
+	printf("%d\n", set_bit(4, 0));
+	
+	printf("%d\n", set_bit(1, 1));
+	printf("%d\n", set_bit(2, 1));
+	printf("%d\n", set_bit(3, 1));
+	printf("%d\n", set_bit(4, 1));
+	printf("%d\n", set_bit(1, 31));
+	
+	printf("%d\n", unset_bit(1, 0));
+	printf("%d\n", unset_bit(2, 0));
+	printf("%d\n", unset_bit(3, 0));
+	printf("%d\n", unset_bit(4, 0));
+	
+	printf("%d\n", unset_bit(1, 1));
+	printf("%d\n", unset_bit(2, 1));
+	printf("%d\n", unset_bit(3, 1));
+	printf("%d\n", unset_bit(4, 1));
+	printf("%d\n", unset_bit(-1, 31));
+	
+	int i;
+    for(i=31;i>=0;--i)
+        printf( "%d", check_bit(0, 32, i) );
+    putchar('\n');
+    for(i=31;i>=0;--i)
+        printf( "%d", check_bit(1, 32, i) );
+    putchar('\n');
+    for(i=31;i>=0;--i)
+        printf( "%d", check_bit(2147483647, 32, i) );
+    putchar('\n');
+    for(i=31;i>=0;--i)
+        printf( "%d", check_bit(-1, 32, i) );
+    putchar('\n');
+    for(i=31;i>=0;--i)
+        printf( "%d", check_bit(-2147483647, 32, i) );
+    putchar('\n');
+}
+
+int main(void) {
+
+    int i, v, bit, isSet;
+    while(scanf("%d %d %d", &v, &bit, &isSet) == 3)
+    {
+        if(isSet)
+            v = set_bit(v, bit);
+        else
+            v = unset_bit(v, bit);
+
+        for(i=31;i>=0;--i)
+            printf( "%d", check_bit(v, 32, i) );
+        putchar('\n');
+    }
+    return 0;
+}
+
