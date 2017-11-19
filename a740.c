274d2481904c7dd91fa0f6a094b30d121db00112
jason3e7 <jason3e7@gmail.com>
2017-11-20 00:00:38 +1300

    a740 AC

--- a740.c
+++ a740.c
@@ -34,9 +34,37 @@ void primeTable() {
 	}
 }
 
+int checkPrime(int n) {
+	if(n < 0) {
+		return -1;
+	} else if(n <= 1) {
+		return 0;
+	}
+	if(n <= 20000000) {
+		if(get(n) == 0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	if(n == 2 || n == 3) {
+		return 1;
+	}
+	if(n % 2 == 0 || n % 3 == 0) {
+		return 0;
+	}
+	int i, temp = (int)sqrt(n) + 1;
+	for(i = 6; i <= temp; i += 6) {
+		if(n % (i - 1) == 0 || n % (i + 1) == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
 int main() {
 	primeTable();
-	int i, prime[1280000] = {0}, point = 1;
+	int i, prime[1270608] = {0}, point = 1;
 	prime[0] = 2;
 	for(i = 3; i <= max; i+=2) {
 		if(get(i) == 0) {
@@ -46,9 +74,13 @@ int main() {
 	}
 	int x, base, sum;
 	while(scanf("%d", &x) != EOF) {
+		if(checkPrime(x) == 1) {
+			printf("%d\n", x);
+			continue;
+		}
 		sum = 0;
 		base = 0;
-		while(x >= 2) {
+		while(x >= 2 && base < 1270608) {
 			while(x % prime[base] == 0) {
 				x /= prime[base];
 				sum += prime[base];
