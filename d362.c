2dd157d26297d37429405da788ae95491d7eaa29
jason3e7 <jason3e7@gmail.com>
2017-11-21 11:38:41 +1300

    d362 AC

---
+++ d362.c
@@ -0,0 +1,55 @@
+ /*
+  * @file d362.c
+  * @author Jason3e7
+  * @algorithm prime
+  * @UVa 10394
+  * @date 201711211136
+  */
+
+#include <stdio.h>
+#include <math.h>
+#define max 20000000
+
+int bitset[(max>>5) + 1] = {0};
+
+int  get(int n) { return bitset[n>>5]>>(n&31)&1; }
+void set(int n) { bitset[n>>5] |= 1<<(n&31); }
+
+void primeTable() {
+	int i, j, k, temp = (max>>5)+1;
+	for(i = 0; i < temp; i++) {
+		bitset[i] = 0;
+	}
+	set(0);
+	set(1);
+	temp = (int)sqrt(max);
+	for(i = 2; i <= temp; i++) {
+		if(get(i) == 0) {
+			for(k = max / i, j = i * k; k >= i; j -= i, k--) {
+				if(get(k) == 0) {
+					set(j);
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	primeTable();
+	int out[100000], i, temp = max - 4, point = 0;
+	for(i = 3; i <= temp; i += 2) {
+		if(get(i) == 0 && get(i + 2) == 0) {
+			out[point] = i;
+			point++;
+		}
+		if(point >= 100000) {
+			break;
+		}
+	}
+	int n;
+	while(scanf("%d", &n) != EOF) {
+		printf("(%d, %d)\n", out[n - 1], out[n - 1] + 2);
+	}
+	return 0;
+}
+
