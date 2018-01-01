dab9a31400de934db6fdcf0cea7635627fae31fd
jason3e7 <jason3e7@gmail.com>
2018-01-02 12:59:05 +1300

    b120 AC

--- b120.c
+++ b120.c
@@ -15,55 +15,24 @@ int g(int n) {
 	return 2;
 }
 
-int map[6] = {2, 5, 5, 2, -1, -1};
-int h(int n) {
-	if(n < 2) {
-		return -1;
-	}
-	return map[(n - 2) % 6];
-}
-
-int out[610];
-int f(int n) {
-	int temp = h(n), temp2;
-	if(n > temp) {
-		return f(n - 1) - temp;
-	}
-	if(n < temp) {
-		temp2 = g(n);
-		return f(temp2) - temp2;
-	}
-	return 1;
-}
-
-
 int fPlus(int n) {
+	int out[9] = {1, 2, 3, 1, -1, -1, -3, -2, -1};
 	if(-1 <= n && n <= 7) {
-		return out[n + 300];
+		return out[n + 1];
 	}
 	int fMap[6] = {2, 7, 12, 14, 13, 12};
-	n -= 8;
-	return out[7 + 300] - 12 * (n / 6) - fMap[n % 6];
+	if(n > 7) {
+		n -= 8;
+		return out[8] - 12 * (n / 6) - fMap[n % 6];
+	}
+	int temp = g(n);
+	return fPlus(temp) - temp;
 }
 
 int main() {
-	int temp, i;
-	out[-1 + 300] = 1;
-	out[0 + 300] = 2;
-	out[1 + 300] = 3;
-	out[2 + 300] = 1;
-	out[3 + 300] = -1;
-	out[4 + 300] = -1;
-	out[5 + 300] = -3;
-	out[6 + 300] = -2;
-	out[7 + 300] = -1;
 	int n;
 	while(scanf("%d", &n) != EOF) {
-		if(-1 <= n && n <= 299) {
-			printf("%d\n", fPlus(n));
-		} else { 
-			printf("%d\n", f(n));
-		}
+		printf("%d\n", fPlus(n));
 	}
 	return 0;
 }
